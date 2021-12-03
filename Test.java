936276db2350677a84b5aa7d3c5f653187a159a0
Daarrik <darrikwh@gmail.com>
2021-12-02 18:43:09 -0800

    foreach

--- Test.java
+++ Test.java
@@ -1,3 +1,7 @@
 public class Test {
-  
+  public static void main(String[] args) {
+    int[] original = new int[]{1,2,3};
+    for (int i : original) i = i * i;
+    for (int i : original) System.out.println(i); 
+  }
 }
